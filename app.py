aa6e1a7f01c0f461e8ab2853f305b4f908179124
Gautam Rangarajan <grrangar@amazon.com>
2023-10-12 00:05:10 -0700

    Adding our first API

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, World!"
+
+@app.route('/api/createRoom')
+def api_greeting():
+    # Sample local endpoint http://127.0.0.1:5000/api/greeting?user=Gautam
+    user = request.args.get('user', 'User')  # 'User' is a default value
+    return jsonify(message=f"Hello {user}!")
