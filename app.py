906bb38e57199ddc576e9d096c6421235afd29a6
Arul Srinivas <det.abu@gmail.com>
2023-10-15 22:54:08 -0700

    Test change from Arul

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def hello():
 def api_greeting():
     # Sample local endpoint http://127.0.0.1:5000/api/greeting?user=Gautam
     user = request.args.get('user', 'User')  # 'User' is a default value
-    return jsonify(message=f"Hello {user}!")
+    return jsonify(message=f"Hello {user}! How are you doing?")
